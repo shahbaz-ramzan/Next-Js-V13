e67a0ce24e0333334679b1e5e4ec53c02334458a
Shahbaz <shahbaz.ramzan@codedistrict.com>
2023-10-17 15:44:54 +0500

    format the code

--- src/app/about/aboutstudent/page.js
+++ src/app/about/aboutstudent/page.js
@@ -3,12 +3,12 @@ import Link from "next/link";
 export default function AboutStudent() {
   return (
     <main>
-        <h1 className="heading">About Student</h1>
-        <br></br>
-        <Link href="/">Go to Home</Link>
-        <br></br>
-        <br></br>
-        <Link href="/about">Go to About</Link>
+      <h1 className="heading">About Student</h1>
+      <br></br>
+      <Link href="/">Go to Home</Link>
+      <br></br>
+      <br></br>
+      <Link href="/about">Go to About</Link>
     </main>
   )
 }
