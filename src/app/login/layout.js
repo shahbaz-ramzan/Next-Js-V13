dd54ecde8783b7a50282620f0738c9236423e99c
Shahbaz <shahbaz.ramzan@codedistrict.com>
2023-10-18 18:46:48 +0500

    middleware in next js

--- src/app/login/layout.js
+++ src/app/login/layout.js
@@ -5,7 +5,6 @@ import { usePathname } from "next/navigation";
 
 export default function Layout({ children }) {
     const pathName = usePathname()
-    console.log(pathName)
     return (
         <div>
             {
---
+++ src/middleware.js
@@ -0,0 +1,14 @@
+
+import { NextResponse } from "next/server"
+export default function middleware(request) {
+  console.log(request)
+
+//   if(request.nextUrl.pathname !== "/login"){
+    return NextResponse.redirect(new URL("/login",request.url)) 
+//   }
+
+
+}
+export const config={
+    matcher:["/about/:path*","/studentlist/:path*"]
+  }
