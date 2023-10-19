3219677d6e9db2fd86c75d6b3d1e7a54da967873
Shahbaz <shahbaz.ramzan@codedistrict.com>
2023-10-19 16:07:35 +0500

    latest code

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,7 @@
 "use client"
 import "./globals.css"
+import other from "./other.module.css"
+import custom from "@/style/custom.module.css"
 import Link from 'next/link'
 
 
@@ -8,7 +10,8 @@ export default function Home() {
     return (
         <main >
             
-            <h1>Fetch Data with API in Client Component</h1>
+            <h1 className={custom.main}>Fetch Data with API in Client Component</h1>
+            <h2 className={other.main}>This heading is for other module to apply css</h2>
             <ul className="navbar">
                 <li>
                    <Link href="/productlist">Go to Product List Page with client side render</Link>
